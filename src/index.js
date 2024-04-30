0c1a2dd4de6c3fc7439b0405e534c0732020f803
Dom Yip <github@yipnet.com>
2024-04-30 11:56:33 -0700

    Code Club Assignment 2

--- src/index.js
+++ src/index.js
@@ -1,28 +1,20 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
-
-
-var src_default = {
+// Code Club Assignment 1
+var assignment_1 = {
 			async fetch(request, env, ctx) {
-				var body = {};
-				var message;
+				let body = {};
+				let message;
+
 				if (request.method == "POST") {
-							message = "Hello World, you did a POST";
+							message = "Hello World, you did a POST!";
 					} 
 				else {
-							message = "Hello World, you did not do a POST";
-						}
-				const output = {
-							result: message,
+							message = "Hello World, you did not do a POST!";
+					}
+
+				const OUTPUT = {
+					result: message,
 				};
-				return new Response (JSON.stringify (output), {
+				return new Response (JSON.stringify (OUTPUT), {
 							headers: {
 								'content-type': 'application/json;charset=UTF-8',
 							},
@@ -30,6 +22,55 @@ var src_default = {
 		}
 }
 
+//Code Club Assignment 2
+var assignment_2 = {
+			async fetch(request, env, ctx) {
+				let something = country_response ("ES");
+
+				const COUNTRY_MAP = {
+						0: "US",
+						1: "FR",
+						2: "ES",
+						3: "DE",
+						4: "JP",
+					}
+
+				let random_pick = Math.floor ((Math.random () * 10)) % 5;
+				let body = {};
+				let message;
+
+				if (request.method == "POST") {
+							message = "Hello World, you did a POST!";
+					} 
+				else {
+							message = "Hello World, you did not do a POST!";
+					}
+
+				const OUTPUT = {
+					result: message + " " + country_response (COUNTRY_MAP[random_pick]),
+					country: COUNTRY_MAP[random_pick],
+				};
+				return new Response (JSON.stringify (OUTPUT), {
+							headers: {
+								'content-type': 'application/json;charset=UTF-8',
+							},
+						});
+		}
+}
+
+// Function for Assignment 2
+function country_response (country) {
+		const COUNTRY_LOOKUP_TABLE = {
+			US: "Thank you for cominng, please come again!",
+			FR: "Merci d'être venu, revenez s'il vous plaît!",
+			ES: "¡Gracias por venir, por favor vuelve!",
+			DE: "„Danke für Ihr Kommen, bitte kommen Sie wieder!",
+			JP: "「来てくれてありがとう、また来てね！」",
+		};
+
+	return (COUNTRY_LOOKUP_TABLE[country]);
+}
+
 export {
-	src_default as default 
+	assignment_2 as default 
 };
